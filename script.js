bf00f0dc9deac7371b360c98c2e9b5f39214735a
Aslamto Ibrahim <aslamtoibrahim@gmail.com>
2023-08-18 11:50:44 +0000

    finish the project

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 function showSocialBtn(){
     const shareIcon = document.getElementById("share-icon");
-    const socialHidden = document.getElementById("social-hidden");
     const icon = document.getElementById("icon");
+    const information = document.getElementById("information");
+    const authorInfo = document.getElementById("author-info");
+    const socialHidden = document.getElementById("social-hidden");
 
-    if (socialHidden.style.display == "none") {
-        socialHidden.style.display = "flex";
-        shareIcon.style.backgroundColor = "hsl(217, 19%, 35%)";
-        icon.src = 'images/icon-share-white.svg'
-        
+    if (screen.width > 800) {
+        if (socialHidden.style.display == "none") {
+            socialHidden.style.display = "flex";
+            shareIcon.style.backgroundColor = "hsl(217, 19%, 35%)";
+            icon.src = 'images/icon-share-white.svg'
+            
+        } else {
+            socialHidden.style.display = "none";
+            shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
+            icon.src = 'images/icon-share.svg'
+             
+        }
     } else {
-        socialHidden.style.display = "none";
-        shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
-        icon.src = 'images/icon-share.svg'
-         
+        if (socialHidden.style.display == "none") {
+            socialHidden.style.display = "flex";
+            authorInfo.style.display = "none";
+            information.style.backgroundColor = "hsl(217, 19%, 35%)";
+            
+        } else {
+            socialHidden.style.display = "none";
+            authorInfo.style.display = "flex";
+            information.style.backgroundColor = "#fff";
+        }
+        
     }
+
+    
 }
